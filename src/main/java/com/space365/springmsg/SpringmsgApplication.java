3b29534626115ea88dd20fa14548e5dc2a1f4c7b
SpaceTech1800 <oliverspace1800@hotmail.com>
2024-11-16 23:51:46 -0800

    first commit

---
+++ src/main/java/com/space365/springmsg/SpringmsgApplication.java
@@ -0,0 +1,13 @@
+package com.space365.springmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/space365/springmsg/SpringmsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space365.springmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
